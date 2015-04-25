6f3010f2cbf44567a4a005f6fad59adc7801bd39
pranaypareek <pranay.metal@gmail.com>
2015-04-26 02:16:15 +0530

    gets kormangala on the map!

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,17 +1,31 @@
 angular.module('pr3d1ctController', [])
 
-	// inject the Todo service factory into our controller
-	.controller('mainController', ['$scope','$http','Predictors', function($scope, $http, Predictors) {
-		$scope.formData = {};
-		$scope.loading = true;
+// inject the Todo service factory into our controller
+.controller('mainController', ['$scope', '$http', 'Predictors', function($scope, $http, Predictors) {
+    $scope.formData = {};
+    $scope.loading = true;
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    // GET =====================================================================
+    // when landing on the page, get all todos and show them
+    // use the service to get all the todos
+    $scope.searchMap = function() {
+      Predictors.search()
+        .success(function(data) {
 
-		// GET =====================================================================
-		// when landing on the page, get all todos and show them
-		// use the service to get all the todos
-		Predictors.search()
-			.success(function(data) {
-        console.log('data=====================', data);
-        $scope.places = data;
-				$scope.loading = false;
-			});
-	}]);
+            $scope.placesGeometryList = [];
+
+
+            data.results.forEach(function(obj) {
+                $scope.placesGeometryList.push(obj.geometry.location);
+            });
+
+            console.log('placesGeometryList: ', $scope.placesGeometryList);
+
+            var lat = $scope.placesGeometryList[0].lat;
+            var lng = $scope.placesGeometryList[0].lng;
+
+            $scope.map = { center: { latitude: lat, longitude: lng }, zoom: 15 };
+            $scope.loading = false;
+        });
+      }
+}]);
--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,1 @@
-angular.module('pr3d1ct-app', ['pr3d1ctController', 'pr3d1ctService']);
+angular.module('pr3d1ct-app', ['pr3d1ctController', 'pr3d1ctService', 'uiGmapgoogle-maps']);
